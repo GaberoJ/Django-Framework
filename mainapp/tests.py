399b5bbd01822e6e02acc4334e4608606b72ac91
maksim <maksim.barabanov2002@mail.ru>
2023-02-13 21:36:27 +0500

    fix

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -137,7 +137,7 @@ class TestCoursesWithMock(TestCase):
         with open("mainapp/fixtures/006_feedback_list_1.bin", "rb") as inpf, mock.patch(
             "django.core.cache.cache.get"
         ) as mocked_cache:
-            mocked_cache.return_value = pickle.load(inpf)
+            mocked_cache.return_value = str(pickle.load(inpf))
             result = self.client.get(path)
             self.assertEqual(result.status_code, HTTPStatus.OK)
             self.assertTrue(mocked_cache.called)
